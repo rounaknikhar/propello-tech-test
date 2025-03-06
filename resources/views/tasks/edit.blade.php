308fce8f39b5c5d295b143827cdbef553822f0cb
Rounak Nikhar <rounakrezanikhar@hotmail.com>
2025-03-06 20:01:31 +0000

    Task 1: Fix updating a task

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,13 +3,19 @@
 @section('content')
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900">
-            <form method="POST" action="{{ route('tasks.update', ['task' =>  $task]) }}">
+            <form method="POST" action="{{ route('tasks.update', ['task' => $task]) }}">
+                {{-- TASK 1 : Fix update task functionality --}}
 
-                <input type="hidden" name="_token" autocomplete="off" value="JbXMTGEI0EuV90PVZPeEm8eC7r45wmQxUQVLG5B2">
+                {{-- Remove static CSRF --}}
+                {{-- <input type="hidden" name="_token" autocomplete="off" value="JbXMTGEI0EuV90PVZPeEm8eC7r45wmQxUQVLG5B2"> --}}
+
+                {{-- Add dynamic CSRF --}}
+                @csrf
 
                 <div class="pb-4">
                     <x-forms.input-label for="name" :value="__('Name')" />
-                    <x-forms.text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="$task->name" required autofocus />
+                    <x-forms.text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="$task->name"
+                        required autofocus />
                     <x-forms.input-error :messages="$errors->get('name')" class="mt-2" />
                 </div>
 
